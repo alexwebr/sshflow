7a838901cc393eee2fba738c82fde7319e9165ba
Alex Weber <alexwebr@gmail.com>
2013-06-15 16:23:59 -0500

    The SCP plugin doesn't know that it's SCP - could be SFTP or something else, too.

--- analyzers/scp.py
+++ analyzers/scp.py
@@ -6,6 +6,6 @@ import cipher
 def analyze(c):
     small = c.ciphersuite().smallestPacket()
     if c.serverLengthMode(2)[0][0] == small and c.clientAverageLength() > 1000 and c.clientTotalLength() / c.serverTotalLength() > 5:
-        print "-> Likely a file copy using SCP, from client to server"
+        print "-> Likely a file copy from client to server"
     if c.clientLengthMode(2)[0][0] == small and c.serverAverageLength() > 1000 and c.serverTotalLength() / c.clientTotalLength() > 5:
-        print "-> Likely a file copy using SCP, from server to client"
+        print "-> Likely a file copy from server to client"
